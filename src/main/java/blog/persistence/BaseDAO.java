3c7bd3f80336dce1ce82c6dfea70e8456d14b97a
Lucca Huijgens <lucca.huijgens@student.hu.nl>
2018-11-17 18:52:36 +0100

    iets

--- src/main/java/blog/persistence/BaseDAO.java
+++ src/main/java/blog/persistence/BaseDAO.java
@@ -1,5 +1,9 @@
 package blog.persistence;
 
 public class BaseDAO {
+private String url="";
+private String user="";
+private String password="";
 
+public BaseDAO() {}
 }
--- src/main/java/blog/persistence/BlogDAO.java
+++ src/main/java/blog/persistence/BlogDAO.java
@@ -5,27 +5,32 @@ import java.util.ArrayList;
 import blog.domain.BlogPage;
 import blog.domain.User;
 
-public class BlogDAO extends BaseDAO{
+public class BlogDAO extends BaseDAO {
 
-	public BlogDAO() {}
-	
-	public ArrayList<BlogPage> getAllBlogs(){
-		
+	public BlogDAO() {
 	}
-	
-	public ArrayList<BlogPage> getBlogsByCategory(String category){
-		
+
+	public ArrayList<BlogPage> getAllBlogs() {
+		ArrayList<BlogPage> list = new ArrayList<BlogPage>();
+
+		return list;
 	}
-	
-	public boolean createBlog(User user,String content) {
-		
+
+	public ArrayList<BlogPage> getBlogsByCategory(String category) {
+		ArrayList<BlogPage> list = new ArrayList<BlogPage>();
+
+		return list;
 	}
-	
+
+	public boolean createBlog(User user, String content) {
+
+	}
+
 	public boolean editBlog(int id, BlogPage blog) {
-		
+
 	}
-	
+
 	public boolean deleteBlog(int id) {
-		
+
 	}
 }
